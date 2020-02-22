1082782a5f1aab625b5d4bbf4972800e81ec528d
Cédryc Ruth <ceruth@domepfc.net>
2020-02-22 10:50:48 +0100

    Get all wines

--- app/routes.php
+++ app/routes.php
@@ -17,13 +17,39 @@ return function (App $app) {
     
     $app->get('/api/wines', function(Request $request, Response $response) {
         //Récupérer les données de la BD
-        $data = include('public/wines.json');
+        //$data = include('public/wines.json');     //Mock
+        
+        //Se connecter au serveur de DB
+        try {
+            $pdo = new PDO('mysql:host=localhost;dbname=cedllar','root','root', [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]);
+        
+            //Préparer la requête
+            $query = 'SELECT * FROM wine';
+        
+            //Envoyer la requête
+            $stmt = $pdo->query($query);
+
+            //Extraire les données
+            $wines = $stmt->fetchAll(PDO::FETCH_ASSOC);     //var_dump($wines); die;
+        } catch(PDOException $e) {
+            $wines = [
+                [
+                    "error" => "Problème de base données",
+                    "errorCode" => $e->getCode(),
+                    "errorMsg" => $e->getMessage(),
+                ]
+            ];
+        }
         
         //Convertir les données en JSON
-        $data = json_encode($data);
+        $data = json_encode($wines);
         
         $response->getBody()->write($data);
-        return $response->withHeader('content-type', 'application/json');
+        return $response
+                ->withHeader('content-type', 'application/json')
+                ->withHeader('charset', 'utf-8');
     });
     
     
